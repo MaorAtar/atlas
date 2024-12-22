ca11f069d4ba8cae66e9aa9495d585e2743853fa
Guy Ezra <guyez1@ac.sce.ac.il>
2024-12-23 00:26:56 +0200

    google places api and photos completed all trips have real photos from google maps

--- atlas/src/view-trip/components/InfoSection.jsx
+++ atlas/src/view-trip/components/InfoSection.jsx
@@ -1,37 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Send } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { GetPlaceDetails } from '@/service/GlobalApi';
 
+const PHOTO_REF_URL = 'https://places.googleapis.com/v1/{NAME}/media?maxHeightPx=1000&maxWidthPx=1000&key=' + import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
 function InfoSection({trip}) {
-  return (
-    <div>
-        <div className="relative h-[340px] w-full overflow-hidden rounded-xl">
-            <img 
-            src="/placeholder 2.png"
-            className="h-[450px] w-full"
-            alt="Travel placeholder"
-            />
-        </div>
-        
-        <div className='flex justify-between items-center'>
-            <div className='my-5 flex flex-col gap-2'>
-                <h2 className='font-bold text-2xl'>{trip?.userSelection?.location?.label}</h2>
-                <div className='flex gap-5'>
-                    <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sx md:text-md'>📅 {trip.userSelection?.numOfDays} Days</h2>
-                    <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sx md:text-md'>💰 {trip.userSelection?.budget} Budget</h2>
-                    <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sx md:text-md'>🥂 Number of Travelers: {trip.userSelection?.traveler}</h2>
-
-                </div>
-            </div>
-        <Button>
-            Share
-            <Send></Send>
-        </Button>
-        </div>
-        
-    </div>
     
-  );
+    const [photoUrl, setPhotoUrl] = useState();
+    useEffect(()=>{
+        trip && GetPlacePhoto();
+    },[trip])
+
+    const GetPlacePhoto = async () => {
+        const data = {
+            textQuery:trip?.userSelection?.location?.label
+        }
+        const result = await GetPlaceDetails(data).then(resp => {
+            console.log(resp.data.places[0].photos[2].name)
+
+            const PhotoUrl = PHOTO_REF_URL.replace('{NAME}', resp.data.places[0].photos[3].name);
+            setPhotoUrl(PhotoUrl);
+        })
+    }
+
+    return (
+        <div>
+            <div>
+                <img 
+                src={photoUrl ? photoUrl : "/placeholder 2.png"}
+                className="h-[400px] w-full rounded-xl"
+                alt="Travel placeholder"
+                />
+            </div>
+            
+            <div className='flex justify-between items-center'>
+                <div className='my-5 flex flex-col gap-2'>
+                    <h2 className='font-bold text-2xl'>{trip?.userSelection?.location?.label}</h2>
+                    <div className='flex gap-5'>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sx md:text-md'>📅 {trip.userSelection?.numOfDays} Days</h2>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sx md:text-md'>💰 {trip.userSelection?.budget} Budget</h2>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sx md:text-md'>🥂 Number of Travelers: {trip.userSelection?.traveler}</h2>
+
+                    </div>
+                </div>
+            <Button>
+                Share
+                <Send></Send>
+            </Button>
+            </div>
+            
+        </div>
+    
+    );
 }
 
 export default InfoSection;
--- atlas/src/view-trip/components/PlaceCardItem.jsx
+++ atlas/src/view-trip/components/PlaceCardItem.jsx
@@ -1,14 +1,32 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MapPinned } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalApi';
 
 
 function PlaceCardItem({place}) {
+
+  const [photoUrl, setPhotoUrl] = useState();
+  useEffect(()=>{
+    place && GetPlacePhoto();
+  },[place])
+
+  const GetPlacePhoto = async () => {
+    const data = {
+      textQuery:place.placeName
+    }
+    const result = await GetPlaceDetails(data).then(resp => {
+      console.log(resp.data.places[0].photos[2].name)
+
+      const PhotoUrl = PHOTO_REF_URL.replace('{NAME}', resp.data.places[0].photos[3].name);
+      setPhotoUrl(PhotoUrl);
+    })
+  }
   return (
     <Link to={'https://www.google.com/maps/search/?api=1&query=' + place?.placeName} target='_blank'>
         <div className='border rounded-xl p-3 mt-2 flex gap-5 hover:scale-105 transition-all hover:shadow-md cursor-pointer'>
-            <img src="/placeholder 2.png" className='w-[100px] h-[100px] rounded-xl'/>
+            <img src={photoUrl ? photoUrl : "/placeholder 2.png"} className='w-[100px] h-[100px] rounded-xl object-cover'/>
 
             <div>
                 <h2 className='font-bold text-lg '>{place.placeName}</h2>
